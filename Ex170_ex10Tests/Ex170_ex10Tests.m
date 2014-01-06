8cb5d673ba0604bfb1fd15faf7080c7ca645af68
saerem21 <nemessisdds>
2014-01-06 16:14:37 +0900

    Initial Commit

---
+++ Ex170_ex10Tests/Ex170_ex10Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex170_ex10Tests.m
+//  Ex170_ex10Tests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex170_ex10Tests : XCTestCase
+
+@end
+
+@implementation Ex170_ex10Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
